c14caaf4e8fd206d51147d026996301f55e90311
martontamas92 <martontamas92@gmail.com>
2019-01-17 18:02:25 +0200

    Update PizzaOrder.java

--- pizza/src/servlet/PizzaOrder.java
+++ pizza/src/servlet/PizzaOrder.java
@@ -2,10 +2,12 @@ package servlet;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class PizzaOrder
@@ -28,16 +30,28 @@ public class PizzaOrder extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String order = request.getParameter("mydata");
 		response.getWriter()
 		.append("<html>")
 		.append("<body>")
+		.append("<h1>"+ order)
+		.append("</h1>")
 		.append("<form method = POST>")
-		.append("<input type = \"text\">")
+		.append("<input name = \"mydata\" type = \"text\">")
 		.append("<input type = \"submit\" value = \"Küld\">")
 		.append("</html>")
 		.append("</form>")
 		.append("</body>")
 		.append("</html>");
+
+		//PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession(true);
+		session.setAttribute("Order",request.toString());
+		//HttpServletRequest session = request.getSession(true);
+		order = (String)session.getAttribute("Order");
+		//response.getWriter().append(order);
+
+
 	}
 
 	/**
@@ -46,6 +60,9 @@ public class PizzaOrder extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		//String order = null;
+		;
+
 	}
 
 }
